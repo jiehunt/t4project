6034940bc16bee6dea5dccaaeec19d8bc93853d5
Jie Liu <jiehunt@github.com>
2018-03-30 16:43:32 +0900

    add get zero func

--- j_get_train.py
+++ j_get_train.py
@@ -55,5 +55,44 @@ def h_get_train():
 
     return
 
+def h_get_zero():
+    DATA_NUMBER = 60000000
+    path_train ='./input/train.csv'
+    path_test = './input/test.csv'
+    dtypes = {
+            'ip'            : 'uint32',
+            'app'           : 'uint16',
+            'device'        : 'uint16',
+            'os'            : 'uint16',
+            'channel'       : 'uint16',
+            'is_attributed' : 'uint8',
+            'click_id'      : 'uint32'
+            }
+
+    path_train_0 ='./input/train_all0.csv'
+    df = pd.read_csv(path_train_0, dtype=dtypes, nrows=range(DATA_NUMBER, 2*DATA_NUMBER))
+    path_train_1 ='./input/train_00.csv'
+    df.to_csv(path_train_1, index=False)
+    del df
+    gc.collect()
+
+    path_train_0 ='./input/train_all0.csv'
+    df = pd.read_csv(path_train_0, dtype=dtypes, nrows=range(1,DATA_NUMBER))
+    path_train_2 ='./input/train_01.csv'
+    df.to_csv(path_train_0, index=False)
+    del df
+    gc.collect()
+
+    path_train_0 ='./input/train_all0.csv'
+    df = pd.read_csv(path_train_0, dtype=dtypes, skiprows=range(1, 2*DATA_NUMBER))
+    path_train_1 ='./input/train_02.csv'
+    df.to_csv(path_train_1, index=False)
+    del df
+    gc.collect()
+
+    return
+
+
 if __name__ == '__main__':
-    h_get_train()
+    # h_get_train()
+    h_get_zero()
