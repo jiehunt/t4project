c292950a09d1fe00834f378b8906bf7044889c3a
jiehunt <jiehunt@github.com>
2018-04-01 12:12:25 +0900

    add get pseudo func

--- j_get_train.py
+++ j_get_train.py
@@ -16,6 +16,19 @@ from scipy.sparse import hstack
 from scipy.sparse import csr_matrix
 from scipy.stats  import rankdata
 
+from contextlib import contextmanager
+
+@contextmanager
+def timer(name):
+    """
+    Taken from Konstantin Lopuhin https://www.kaggle.com/lopuhin
+    in script named : Mercari Golf: 0.3875 CV in 75 LOC, 1900 s
+    https://www.kaggle.com/lopuhin/mercari-golf-0-3875-cv-in-75-loc-1900-s
+    """
+    t0 = time.time()
+    yield
+    print(f'[{name}] done in {time.time() - t0:.0f} s')
+
 
 def h_get_train():
     DATA_NUMBER = 40000000
@@ -111,7 +124,42 @@ def h_get_zero():
     # gc.collect()
     return
 
+def h_get_pseudo_data(path_sub):
+    path_test = './input/test.csv'
+    test_cols = ['ip', 'app', 'device', 'os', 'channel', 'click_time']
+    sub_cols = ['is_attributed']
+
+    dtypes = {
+            'ip'            : 'uint32',
+            'app'           : 'uint16',
+            'device'        : 'uint16',
+            'os'            : 'uint16',
+            'channel'       : 'uint16',
+            'is_attributed' : 'uint8',
+            'click_id'      : 'uint32'
+            }
+
+    with timer('Loading the submission data...'):
+        sub = pd.read_csv(path_sub, header=0, usecols=sub_cols)
+
+    sub['is_attributed'] = sub['is_attributed'].apply(lambda x: 1 if x >= 0.5 else 0 )
+
+    with timer('Loading the test data...'):
+        test = pd.read_csv(path_test, dtype=dtypes, header=0, usecols=test_cols)
+
+    pseudo_df = pd.concat([test, sub],axis=1)
+    print (pseudo_df.info())
+    print (pseudo_df.head())
+
+    outfile = 'pseudo/' + 'pseudo.csv'
+    pseudo_df.to_csv(outfile, index=False, float_format="%.6f")
+
+    return
 
 if __name__ == '__main__':
     # h_get_train()
-    h_get_zero()
+    # h_get_zero()
+    with timer('goto prepare pseudo file ... '):
+        path_sub = 'output/set20lgbandy_org.csv'
+        h_get_pseudo_data(path_sub)
+
